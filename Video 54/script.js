56ddd0bb7d18de8a3feb51968ca0bc866f46d61c
Talentkiller04 <ganubhav162@gmail.com>
2024-01-20 13:36:33 +0530

    sigma

--- Video 54/script.js
+++ Video 54/script.js
@@ -1,22 +1,15 @@
-alert("Hello World");
+alert("Hello guys");
+console.log("Hello world");
+var a = prompt("Enter you name");
+if(a)
+{
+    console.log("yeah he wrote something");
 
-console.log("Code is running...")
-console.log("Code is also running...")
-console.log("Code is looking like a wow...")
-
-var a = prompt("Enter your number")
-var isTrue = confirm("Are you sure you want to leave this page and blast your computer ")
-
-if(isTrue){
-    console.log("Computer is blasting")
-}
-
-else{
-    console.log("Computer is not blasting")
 
 }
-console.log("Your number is " + a)
-
-document.title = "Hey I am good"
-
-// document.body.style.backgroundColor = "red"
+else
+{
+    console.log("no he didn't wrote something");
+}
+console.log("your input was",a);
+document.title="Hello";
